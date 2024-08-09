431e62b7425271775eb0a591266f902257a60743
Alex Figliolia <alexfigliolia@gmail.com>
2024-08-09 10:28:07 -0400

    @import => @use

---
+++ src/declarations/module-scss.d.ts
@@ -0,0 +1,8 @@
+declare module "Styles/exports.module.scss" {
+  class Styles {
+    public static smallScreenScale: string;
+    public static largeScreenScale: string;
+    public static screenInnerTransition: string;
+  }
+  export default Styles;
+}
